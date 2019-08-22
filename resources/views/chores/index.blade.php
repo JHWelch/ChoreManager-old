fbbe5d588239ad1825e67da72724520d22a1ea78
Jordan Welch <JordanHWelch@gmail.com>
2019-08-22 10:19:47 -0500

    Saving progress

--- resources/views/chores/index.blade.php
+++ resources/views/chores/index.blade.php
@@ -1,25 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Chores</div>
+    @component('components.maincontainer')
+        @component('components.card')
+            @slot('header')
+            Chores
+            @endslot
+                <ul>
+                    @foreach ($chores as $chore)
+                    <li><a href="/chores/{{ $chore->id }}"> {{ $chore->title }}</a></li>
+                    @endforeach
+                </ul>
 
-                <div class="card-body">
-                    <ul>
-                        @foreach ($chores as $chore)
-                        <li><a href="/chores/{{ $chore->id }}"> {{ $chore->title }}</a></li>
-                        @endforeach
-                    </ul>
-
-                    <div>
-                        <a class="btn btn-primary" href="/chores/create" title="Create New Chore Link">Create New Chore</a>
-                    </div>
+                <div>
+                    <a class="btn btn-primary" href="/chores/create" title="Create New Chore Link">Create New Chore</a>
                 </div>
-            </div>
-        </div>
-    </div>
-</div>
+        @endcomponent
+    @endcomponent
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,4 @@
-<?php
+\<?php
 
 /*
 |--------------------------------------------------------------------------
@@ -9,14 +9,21 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get(
+    '/',
+    function () {
+        return view('welcome');
+    }
+);
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('chores', 'ChoresController')->middleware('auth');
+
+Route::get('/digest/day', 'DigestController@day')->middleware('auth');
+Route::get('/digest/week', 'DigestController@week')->middleware('auth');
+Route::get('/digest/month', 'DigestController@month')->middleware('auth');
